c081544ec670887d528bfafce23f49ba41d03e43
Rafa Tárrega <rafatarre@gmail.com>
2022-02-12 08:44:49 +0100

    feat: add navbar menu

---
+++ src/components/NavbarMenu.js
@@ -0,0 +1,28 @@
+import { Container, Navbar } from "react-bootstrap";
+
+export default function NavbarMenu() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#!">
+            Dynamic <b>Form</b>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as:{" "}
+              <a
+                href="https://rafatarrega.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Rafa Tárrega
+              </a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
